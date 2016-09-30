95266700e46f36c67df8aa2ffd4efa5b40c62cad
Jason Buss <jasonmbuss@gmail.com>
2016-09-30 16:36:38 -0500

    making circular button

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-// $(document).ready(function(){
-//     $('.project-display').hover(function() {
-//         $(this).css({"opacity":"1", "z-index":"2"});
-//         $(this).addClass('transition');
-//         $('.project',this).addClass('inverse-transition');
-//     },
-//     function() {
-//         $(this).css({"opacity":".5", "z-index":"0"});
-//         $(this).removeClass('transition');
-//         $('.project',this).removeClass('inverse-transition');
-//     });
-//   });
-
-
 $(document).ready(function(){
     $('.project-display').hover(function() {
         $(this).css({"opacity":"1", "z-index":"2"});
+        $('.button',this).css("opacity", "1");
         $(this).addClass('translate');
         $('.project',this).addClass('translate-inverse');
-        // $(this).animate({"transform": "translate3d(0px,0px,1000px)"});
-        // $(this).siblings().animate({"height": "32%"});
     },
     function() {
         $(this).css({"opacity":".5", "z-index":"0"});
+        $('.button',this).css("opacity", ".25");
         $(this).removeClass('translate');
         $('.project',this).removeClass('translate-inverse');
-        // $(this).animate({"transform": "translate3d(0px,0px,0px)"});
-        // $(this).siblings().animate({"height": "33.33333333%"},0.5);
     });
   });
+
+$(document).ready(function(){
+    $('.button').hover(function() {
+        $(this).css({"outline": "2px solid rgba(255,255,255,.5)", "outline-offset": "5px"});
+    },
+    function() {
+        $(this).css({"outline": "0px solid white", "outline-offset": "0px"});
+    });
+  });
+
